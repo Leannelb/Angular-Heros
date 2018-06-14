2829f01ff185e2596d93252378db535f9fb0e674
leanne <>
2018-06-14 12:59:12 +0200

    myself and Karolis added some functionalities this morning like on click - and hover

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,14 +8,27 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
- 
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+  heroes: Hero[] = HEROES;
+  selectedHero: Hero;
+
+
   constructor() { }
  
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    alert('a new hero was selected ' + this.selectedHero.name);
+    console.log('hfkjahfkjhadkjfh');
+  }
+
+  onHoover(hero: Hero): void {
+    // alert('hover hero ' + hero.name + ' ' + hero.id);
   }
  
 }
