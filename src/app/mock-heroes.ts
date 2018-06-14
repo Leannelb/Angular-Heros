a5680eea334311227b5808262e142adcc8f35dda
leanne <>
2018-06-14 09:37:37 +0200

    3, The hero edito complete

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Mr. Nice' },
+    {id: 12, name: 'Narco' },
+    {id: 13, name: 'Bombasto' },
+    {id: 14, name: 'Celeratis' },
+    {id: 14, name: 'Magenta' },
+    {id: 15, name: 'RubberMan' },
+    {id: 16, name: 'Dynama' },
+    {id: 17, name: 'Dr IQ' },
+    {id: 18, name: 'Marple' },
+    {id: 19, name: 'Lady' },
+    {id: 20, name: 'Lola' },
+
+];
