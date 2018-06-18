4aceb8da68071e7a7f610d97e508cba1ba5f0c5d
leanne <>
2018-06-18 12:59:33 +0200

    moving onto step 6

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,27 +8,48 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Windstorm'
-  // };
-  heroes: Hero[] = HEROES;
+
+  heroes = HEROES;
+
   selectedHero: Hero;
 
 
   constructor() { }
- 
+
   ngOnInit() {
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    alert('a new hero was selected ' + this.selectedHero.name);
-    console.log('hfkjahfkjhadkjfh');
   }
-
-  onHoover(hero: Hero): void {
-    // alert('hover hero ' + hero.name + ' ' + hero.id);
-  }
- 
 }
+
+
+
+// Old code with notes
+//export class HeroesComponent implements OnInit {
+//   // hero: Hero = {
+//   //   id: 1,
+//   //   name: 'Windstorm'
+//   // };
+//   heroes: Hero[] = HEROES;
+//   selectedHero: Hero;
+//   //define selected hero is of type hero I THINK
+
+
+//   constructor() { }
+ 
+//   ngOnInit() {
+//   }
+
+//   onSelect(hero: Hero): void {
+//     this.selectedHero = hero;
+//     alert('a new hero was selected ' + this.selectedHero.name);
+//     console.log('hfkjahfkjhadkjfh');
+//   }
+
+//   onHoover(hero: Hero): void {
+//     // alert('hover hero ' + hero.name + ' ' + hero.id);
+//   }
+ 
+// }
